b6a0ef2e95a5d28c5ad93cdf3b0718ad78135804
ayoubelhilali <elhilaliayoub2020@gmail.com>
2025-05-08 15:20:44 +0100

    login error fixed

--- traitement.php
+++ traitement.php
@@ -1,21 +1,29 @@
 <?php 
 
-$email= $_POST['email'] ;
-$password = $_POST['password'] ;
-
+$email= trim($_POST['email']) ;
+$password = trim($_POST['password']) ;
+echo $email;
 $pdo = new PDO('mysql:host=localhost;port=3307;dbname=ensah_service', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- 
-$stmt = $pdo->prepare("SELECT * FROM admin where email=?") ;
-$stmt->execute([$email]) ;
-print_r($stml);
-$user = $stmt->fetch() ;
-
-if(!$user || $user['password']!=$password){
-    header("Location: login.php?message=Email+ou+mot+de+passe+invalide") ;
-    exit() ;
+if ($pdo) {
+    $users_data = "SELECT * FROM admin WHERE email = ?";
+    $stmt = $pdo->prepare($users_data);
+    $stmt->execute([$email]);
+    if ($stmt->rowCount() > 0) {
+        echo "done!";
+    }else {
+        echo "no user found!";
+    }
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    print_r($user);
+    if (!$user || $user['password'] != $password) {
+        header("Location: login.php?message=Email+ou+mot+de+passe+invalide+$user");
+        exit();
+    }
+}else {
+    echo "failed to connect";
 }
-// If login is successful, redirect to the dashboard or another page
+
 header("Location: dashboard/admin-dash.php");
 exit();
 ?>
