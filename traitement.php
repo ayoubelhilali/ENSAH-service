9f5e1467ad59f2bcafab53012196de4861016881
ayoubelhilali <elhilaliayoub2020@gmail.com>
2025-05-08 18:21:32 +0100

    traitement eror fixed

--- traitement.php
+++ traitement.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 $email= trim($_POST['email']) ;
@@ -23,23 +22,3 @@ if ($pdo) {
 $_SESSION['user']= $user["admin_ID"];
 header("Location: dashboard/admin-dash.php");-
 exit();
-=======
-<?php 
-
-$email= $_POST['email'] ;
-$password = $_POST['password'] ;
-
-$pdo = new PDO('mysql:host=localhost;dbname:ensah_service.sql','root','');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$stmt = $pdo->prepare("SELECT password FROM professeur where email=?") ;
-$stmt->execute([$email]) ;
-$user = $stmt->fetch() ;
-
-if(!$user || $user['password']!=$password){
-    header("Location: login.php?message=Email+ou+mot+de+passe+invalide") ;
-    exit() ;
-}
- 
->>>>>>> 6c6e07cdf7c8b07bdc01a0058065878f6d01a090
-?>
